5cda243a4e2e4be13d0ba74eeb500978ab459a8b
leave8080 <1547123709@qq.com>
2020-05-23 00:49:19 +0800

    for range

---
+++ forrange坑/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type student struct {
+	Name string
+	Age  int
+}
+
+func main() {
+	m := make(map[string]*student)
+	stus := []student{
+		{Name: "zhao1", Age: 12},
+		{Name: "zhao2", Age: 13},
+		{Name: "zhao3", Age: 14},
+	}
+	for _, stu := range stus {
+		m[stu.Name] = &stu
+	}
+	for _, v := range m {
+		fmt.Println(v.Name, v.Age)
+	}
+
+	fmt.Println("===========")
+	for i := 0; i < len(stus); i++ {
+		m[stus[i].Name] = &stus[i]
+	}
+	for _, v := range m {
+		fmt.Println(v.Name, v.Age)
+	}
+}
