f01467c137035e445397569b8f95e02496df1850
leave8080 <1547123709@qq.com>
2020-05-24 21:43:32 +0800

    for range 坑

--- slice+forrange/slice/1/main.go
+++ slice+forrange/slice/1/main.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+	nums := [3]int{}
+	nums[0] = 1
+
+	fmt.Printf("nums: %v , len: %d, cap: %d\n", nums, len(nums), cap(nums))
+
+	dnums := nums[0:2]
+	dnums[0] = 5
+
+	fmt.Printf("nums: %v ,len: %d, cap: %d\n", nums, len(nums), cap(nums))
+	fmt.Printf("dnums: %v, len: %d, cap: %d\n", dnums, len(dnums), cap(dnums))
 }
