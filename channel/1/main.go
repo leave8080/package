878c497dad37e7ff45e7cb88e680a5ff3ce456e4
吴乃巍 <44368530+leave8080@users.noreply.github.com>
2020-06-16 11:50:54 +0800

    windows

---
+++ channel/1/main.go
@@ -0,0 +1,26 @@
+package main
+
+func worker(ch chan struct{}) {
+	// Receive a message from the main program.
+	println("3")
+	<-ch
+	println("roger2")
+
+	// Send a message to the main program.
+	close(ch)
+}
+
+func main() {
+	ch := make(chan struct{})
+	go worker(ch)
+	println("1")
+	// Send a message to a worker.
+	ch <- struct{}{}
+	println("2")
+	// Receive a message from the worker.
+	<-ch
+	println("roger")
+	// Output:
+	// roger
+	// roger
+}
