06a06a94ac59ad746de07665185693cd4670796b
吴乃巍 <wunw@mxchip.com>
2020-12-29 14:42:34 +0800

    test

--- test1/main.go
+++ test1/main.go
@@ -2,34 +2,35 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
-	"sync"
+	"net/http"
 )
 
 func main() {
-	var wg sync.WaitGroup
-
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-
-		go func() {
-			defer wg.Done()
-			for j := 0; j < 10; j++ {
-				go test()
-			}
-		}()
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://account.daocloud.io/account-template/signin", nil)
+	if err != nil {
+		log.Fatal(err)
 	}
-	wg.Wait()
-	DeferFunc()
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("Pragma", "no-cache")
+	req.Header.Set("Cache-Control", "no-cache")
+	req.Header.Set("Accept", "application/json, text/plain, */*")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
+	req.Header.Set("Sec-Fetch-Site", "same-origin")
+	req.Header.Set("Sec-Fetch-Mode", "cors")
+	req.Header.Set("Sec-Fetch-Dest", "empty")
+	req.Header.Set("Referer", "https://account.daocloud.io/signin?ret=aHR0cHM6Ly9kYXNoYm9hcmQuZGFvY2xvdWQuaW8vY2x1c3Rlcg^%^3D^%^3D")
+	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8")
+	req.Header.Set("Cookie", "_ga=GA1.2.1391029775.1603447386; marketing_referrer=Direct; gr_user_id=edeaf6e0-586f-4644-adf5-612889755b3f; Hm_lvt_11dbf453d8b02449a4a52199679ea61e=1604390133,1604390181")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", bodyText)
 }
-func test() {
-	fmt.Println("go")
-}
-func DeferFunc() {
-	defer func() {
-		fmt.Println("defer")
-	}()
-	log.Fatal("fatal")
-}
-
-//7， 设计一段代码限制goroutine的数量，比如有100个并行任务，将goroutine的数量限制在十个。
