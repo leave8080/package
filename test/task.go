8f234b2dbd4b18d41820fca1844c2340ee4492ee
leave <wunaiwei8080@gmail.com>
2021-03-29 14:41:47 +0800

    max int

--- test/task.go
+++ test/task.go
@@ -1,46 +1,36 @@
 package main
 
-import (
-	"crypto/md5"
-	"encoding/hex"
-	"fmt"
-)
+import "fmt"
 
-type EntryType int32
-
-const (
-	EntryNomal EntryType = 0
-	Entrydada  EntryType = 1
-)
-
-type Sss struct {
-	Enrt EntryType
+type Push interface {
+	PushCOde()
+	Fs
 }
 
+type Fs interface {
+}
+type SMs struct {
+}
+type Mob struct {
+}
+
+func (this *SMs) PushCOde() {
+	fmt.Println("sms")
+}
+
+func (this *Mob) PushCOde() {
+	fmt.Println("mob")
+
+}
 func main() {
-	var j int
-	tt := []int{2, 4, 6, 7, 8, 11, 14, 16, 19, 23, 24}
-	for i := 0; i < 25; i += 3 {
-		fmt.Println(i)
-		if i == 0 {
-			continue
-		}
-
-		if tt[j] <= i {
-			//fmt.Println(tt[j])
-		}
-
-		j++
+	s := &SMs{}
+	m := &Mob{}
+	//var key int
+	key := 1
+	switch key {
+	case 1:
+		s.PushCOde()
+	case 2:
+		m.PushCOde()
 	}
-
-	fmt.Println(CheckSign("ddc82e40f2183d280e5eeb29508550d9", "b0baae0630f444b0811ea3c2eb212170", "/app/v1/auth/autoLogin", "1615889127"))
-}
-
-func CheckSign(sign, appid, path, ts string) (ok bool) {
-	hash := md5.New()
-	hash.Write([]byte(appid))
-	hash.Write([]byte(path))
-	hash.Write([]byte(ts))
-	calSign := hex.EncodeToString(hash.Sum(nil))
-	return sign == calSign
 }
